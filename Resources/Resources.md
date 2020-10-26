13a861e97e361fd3fd656f84d9a1d9c085eda78a
Bogdan Chisalescu <bogdan.chisalescu@gmail.com>
2020-10-26 21:47:19 +0200

    Extracted the samples from a Raw object and plotted one of the EEG signals with respect to time

--- Resources/Resources.md
+++ Resources/Resources.md
@@ -4,12 +4,14 @@
 - Analyzing neural time series data : theory and practice / Mike X Cohen / The MIT Press
 
 
- ## Websites
+ ## Websites & Online Documentations
   - [Computational Linguistics, The KARA ONE Database](http://www.cs.toronto.edu/~complingweb/data/karaOne/karaOne.html)
 
   - [10 EEG Books Worth Reading by Bryn Farnsworth, Ph.D](https://imotions.com/blog/eeg-books/)
 
   - [MNE-Python software for exploring, visualizing, and analyzing human neurophysiological data such as MEG, EEG, sEEG, ECoG, and more](https://pypi.org/project/mne/)
+
+ - [Extracting data from Raw objects - MNE-Python](https://mne.tools/dev/auto_tutorials/raw/plot_10_raw_overview.html#time-sample-number-and-sample-index)
 
 
   ## YouTube videos
--- Source/SignalPlot.py
+++ Source/SignalPlot.py
@@ -1,11 +1,33 @@
 import mne
 import os
+import matplotlib.pyplot as plt
 
 cwdPath = os.getcwd()
 cwdPath = cwdPath.replace("Source", "Data")
-fileItem = "/imagined_speech_MM05_00_tag1.raw.fif"
+fileItem = "/imagined_speech_MM05_10_tag3.raw.fif"
 fName = cwdPath + fileItem
 raw = mne.io.read_raw_fif(fName)
-raw.plot(duration=5, n_channels=30, block=True)
-print(raw)
 
+#plotting without "block=true" beacuse it blocks execution in Linux
+raw.plot(duration=5, n_channels=30)
+print(raw.info)
+
+#number of samples
+print(len(raw))
+
+#extracting the samples of a certain signal as a tuple
+raw_selection = raw["AF3", 0:len(raw)]
+
+
+print(type(raw_selection))
+print(raw_selection[1])
+
+#plottin one of the signals
+plt.figure()
+plt.title("One of the signals")
+plt.xlabel("Time [s]")
+plt.ylabel("Amplitude [uV]")
+x = raw_selection[1]
+#samples array must be transposed due to the way it is stored 
+y = raw_selection[0].T
+plt.plot(x, y)
