7cb981000a2fa239e4d25a4971d325a925ad2c61
bogdanChisalescu <bogdan.chisalescu@gmail.com>
2021-01-20 00:23:40 +0200

    Backprop still a mess

--- Source/backpropagation.py
+++ Source/backpropagation.py
@@ -12,8 +12,10 @@ def back_propagation(Z_list, A_list):
     wPath = cwdPath.replace("Source", "Weights")
     
     Y = np.loadtxt(lPath + "/labels", dtype='f', delimiter=',')
+    #Y = Y.reshape(Y.shape[0], 1)
+    print(Y.shape)
     
-    L = len(Z_list)
+    L = len(Z_list) + 1
     
     #number of training examples
     m = Y.shape[0]
@@ -22,32 +24,50 @@ def back_propagation(Z_list, A_list):
     db_list = list()
     dZ_old = 0
     
-    for i in range(L-1, -1, -1):
-        if i == L-1:
+    #start from L-2 because weights start at 0
+
+    for i in range(L-2, -1, -1):
+        #first index is start - 1
+        print(str(i) + "for")
+        
+        #last layer special case, w starts at 0
+        if i == L - 2:
+            print("if" + str(i))
             dZ = A_list[i] - Y
             dW = 1/m * np.dot(dZ, A_list[i-1].T)
-            db = 1/m * np.sum(dZ, axis = 0, keepdims=True)
+            try:
+                db = 1/m * np.sum(dZ, keepdims=True )
+            except:
+                dZ = dZ.reshape(dZ.shape[0], 1)
+                db = 1/m * np.sum(dZ, keepdims=True )
             
-            print(dW.shape)
-            print(i)
-            #Appending derivatives
-            dW_list = dW_list.append(dW)
-            db_list = db_list.append(db)
+            #first element is last weight
+            dW_list.append(dW)
+            db_list.append(db)
+            
+            #dZ superscript[i+1]
             dZ_old = dZ
+            
         else:
-            W = np.loadtxt(wPath + "/w" + str(i), dtype='f', delimiter=',')
             print(i)
-            dZ = np.dot(W.T, dZ_old) * tanh_derivative(Z_list[i])
-            dW = 1/m * np.dot(dZ, A_list[i-1].T)
-            db = 1/m * np.sum(dZ, axis = 0, keepdims=True)
-            dZ_old = dZ
+            W = np.loadtxt(wPath + "/w" + str(i), dtype='f', delimiter=',')
+            try:
+                dZ = np.dot(W.T, dZ_old) * tanh_derivative(Z_list[i])
+            except:
+                dZ_old = dZ_old.reshape(dZ_old.shape[0], 1)
+                dZ = np.multiply(np.dot(W.T, dZ_old) , tanh_derivative(Z_list[i]) ) 
+                dW = 1/m * np.dot(dZ, A_list[i-1].T) 
+                db = 1/m * np.sum(dZ, axis = 1, keepdims=True )
+        
+                #first element is last weight
+                dW_list.append(dW)
+                db_list.append(db)
+                
+                #dZ superscript[i+1]
+                dZ_old = dZ
             
-            print(dW.shape)
-            #Appending derivatives
-            dW_list = dW_list.append(dW)
-            db_list = db_list.append(db)
     
-    return (dW, db)
+    return (dW_list, db_list)
     
     end = time.time()
     print("Elapsed time [s]: " + str(end- start))
